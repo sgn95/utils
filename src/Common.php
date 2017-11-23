cb25cc186ce685e9f51269f7100932a48f4046e7
phpfrid <824431999@qq.com>
2017-11-23 15:08:01 +0800

    #

---
+++ src/Common.php
@@ -0,0 +1,14 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: feng
+     * Date: 2017/11/23
+     * Time: 15:07
+     */
+
+    namespace artecfeng\utils;
+
+
+    class Common {
+
+    }
